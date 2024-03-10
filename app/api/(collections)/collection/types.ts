8ac00bddd64d6b4dda8b3c554c0a48bce5a58803
Abdulaziz-Mirsagatov <mirsagah@mail.uc.edu>
2024-03-10 18:01:38 -0400

    post collection req type fix

--- app/api/(collections)/collection/types.ts
+++ app/api/(collections)/collection/types.ts
@@ -1,5 +1,6 @@
 import { Collection } from "@/types/env";
+import { NextRequest } from "next/server";
 
-export interface PostCollectionRequest {
+export interface PostCollectionRequest extends NextRequest {
   json: () => Promise<Collection>;
 }
